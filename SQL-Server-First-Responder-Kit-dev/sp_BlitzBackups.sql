d8d7006dae8378e829214d89636cfbfdac1780ad
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-24 09:57:23 +0530

    Update sp_BlitzBackups.sql

--- SQL-Server-First-Responder-Kit-dev/sp_BlitzBackups.sql
+++ SQL-Server-First-Responder-Kit-dev/sp_BlitzBackups.sql
@@ -2,7 +2,7 @@ IF OBJECT_ID('dbo.sp_BlitzBackups') IS NULL
   EXEC ('CREATE PROCEDURE dbo.sp_BlitzBackups AS RETURN 0;');
 GO
 ALTER PROCEDURE [dbo].[sp_BlitzBackups] 
-    @Help TINYINT = 0 ,
+    @Help TINYINT = 0 , 
 	@HoursBack INT = 168,
 	@MSDBName NVARCHAR(256) = 'msdb', 
 	@AGName NVARCHAR(256) = NULL, 
