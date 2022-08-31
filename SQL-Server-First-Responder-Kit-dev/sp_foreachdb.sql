5f4363f359aaf6b2d5fccf355bdee3366a4a6ece
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-08-31 22:36:20 +0530

    Update sp_foreachdb.sql

--- SQL-Server-First-Responder-Kit-dev/sp_foreachdb.sql
+++ SQL-Server-First-Responder-Kit-dev/sp_foreachdb.sql
@@ -8,7 +8,7 @@ GO
 ALTER PROCEDURE dbo.sp_foreachdb 
     -- Original fields from sp_MSforeachdb...
     @command1 NVARCHAR(MAX) = NULL,
-    @replacechar NCHAR(1) = N'?' ,
+    @replacechar NCHAR(1) = N'?' , 
     @command2 NVARCHAR(MAX) = NULL ,
     @command3 NVARCHAR(MAX) = NULL ,
     @precommand NVARCHAR(MAX) = NULL ,
